c7e040e38b68308ef9b8642fc1729840a4dc0818
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2025-09-01 23:14:09 +0200

    Enhance request handling with new types and functions
    Added types and functions to handle headers, cookies, and params in Next.js with support for migration to Promise-based API.

--- next/types/request/functions.ts
+++ next/types/request/functions.ts
@@ -1,4 +1,42 @@
 import { type ReadonlyRequestCookies } from '../web/spec-extension/adapters/request-cookies';
+import { type ReadonlyHeaders } from '../web/spec-extension/adapters/headers';
+import { type WorkStore } from '../app-render/work-async-storage.external';
+import type { Params } from './params';
+
+
+/** 
+ * In this version of Next.js `headers()` returns a Promise however you can still reference the properties of the underlying Headers instance
+ * synchronously to facilitate migration. The `UnsafeUnwrappedHeaders` type is added to your code by a codemod that attempts to automatically
+ * updates callsites to reflect the new Promise return type. There are some cases where `headers()` cannot be automatically converted, namely
+ * when it is used inside a synchronous function and we can't be sure the function can be made async automatically. In these cases we add an
+ * explicit type case to `UnsafeUnwrappedHeaders` to enable typescript to allow for the synchronous usage only where it is actually necessary.
+ *
+ * You should should update these callsites to either be async functions where the `headers()` value can be awaited or you should call `headers()`
+ * from outside and await the return value before passing it into this function.
+ *
+ * You can find instances that require manual migration by searching for `UnsafeUnwrappedHeaders` in your codebase or by search for a comment that
+ * starts with `@next-codemod-error`.
+ *
+ * In a future version of Next.js `headers()` will only return a Promise and you will not be able to access the underlying Headers instance
+ * without awaiting the return value first. When this change happens the type `UnsafeUnwrappedHeaders` will be updated to reflect that is it no longer
+ * usable.
+ *
+ * This type is marked deprecated to help identify it as target for refactoring away.
+ *
+ * @deprecated
+ */
+export type UnsafeUnwrappedHeaders = ReadonlyHeaders;
+/**
+ * This function allows you to read the HTTP incoming request headers in
+ * [Server Components](https://nextjs.org/docs/app/building-your-application/rendering/server-components),
+ * [Server Actions](https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions-and-mutations),
+ * [Route Handlers](https://nextjs.org/docs/app/building-your-application/routing/route-handlers) and
+ * [Middleware](https://nextjs.org/docs/app/building-your-application/routing/middleware).
+ *
+ * Read more: [Next.js Docs: `headers`](https://nextjs.org/docs/app/api-reference/functions/headers)
+ */
+export declare function headers(): Promise<ReadonlyHeaders>;
+
 /**
  * In this version of Next.js `cookies()` returns a Promise however you can still reference the properties of the underlying cookies object
  * synchronously to facilitate migration. The `UnsafeUnwrappedCookies` type is added to your code by a codemod that attempts to automatically
@@ -30,3 +68,89 @@ export declare function cookies(): Promise<ReadonlyRequestCookies>;
  * During prerendering it will never resolve and during rendering it resolves immediately.
  */
 export declare function connection(): Promise<void>;
+
+export type SearchParams = {
+    [key: string]: string | string[] | undefined;
+};
+/**
+ * In this version of Next.js the `params` prop passed to Layouts, Pages, and other Segments is a Promise.
+ * However to facilitate migration to this new Promise type you can currently still access params directly on the Promise instance passed to these Segments.
+ * The `UnsafeUnwrappedSearchParams` type is available if you need to temporarily access the underlying params without first awaiting or `use`ing the Promise.
+ *
+ * In a future version of Next.js the `params` prop will be a plain Promise and this type will be removed.
+ *
+ * Typically instances of `params` can be updated automatically to be treated as a Promise by a codemod published alongside this Next.js version however if you
+ * have not yet run the codemod of the codemod cannot detect certain instances of `params` usage you should first try to refactor your code to await `params`.
+ *
+ * If refactoring is not possible but you still want to be able to access params directly without typescript errors you can cast the params Promise to this type
+ *
+ * ```tsx
+ * type Props = { searchParams: Promise<{ foo: string }> }
+ *
+ * export default async function Page(props: Props) {
+ *  const { searchParams } = (props.searchParams as unknown as UnsafeUnwrappedSearchParams<typeof props.searchParams>)
+ *  return ...
+ * }
+ * ```
+ *
+ * This type is marked deprecated to help identify it as target for refactoring away.
+ *
+ * @deprecated
+ */
+export type UnsafeUnwrappedSearchParams<P> = P extends Promise<infer U> ? Omit<U, 'then' | 'status' | 'value'> : never;
+export declare function createSearchParamsFromClient(underlyingSearchParams: SearchParams, workStore: WorkStore): Promise<SearchParams>;
+export declare const createServerSearchParamsForMetadata: typeof createServerSearchParamsForServerPage;
+export declare function createServerSearchParamsForServerPage(underlyingSearchParams: SearchParams, workStore: WorkStore): Promise<SearchParams>;
+export declare function createPrerenderSearchParamsForClientPage(workStore: WorkStore): Promise<SearchParams>;
+/**
+ * This is a variation of `makeErroringExoticSearchParams` that always throws an
+ * error on access, because accessing searchParams inside of `"use cache"` is
+ * not allowed.
+ */
+export declare function makeErroringSearchParamsForUseCache(workStore: WorkStore): Promise<SearchParams>;
+
+
+export type ParamValue = string | Array<string> | undefined;
+export type Params = Record<string, ParamValue>;
+/**
+ * In this version of Next.js the `params` prop passed to Layouts, Pages, and other Segments is a Promise.
+ * However to facilitate migration to this new Promise type you can currently still access params directly on the Promise instance passed to these Segments.
+ * The `UnsafeUnwrappedParams` type is available if you need to temporarily access the underlying params without first awaiting or `use`ing the Promise.
+ *
+ * In a future version of Next.js the `params` prop will be a plain Promise and this type will be removed.
+ *
+ * Typically instances of `params` can be updated automatically to be treated as a Promise by a codemod published alongside this Next.js version however if you
+ * have not yet run the codemod of the codemod cannot detect certain instances of `params` usage you should first try to refactor your code to await `params`.
+ *
+ * If refactoring is not possible but you still want to be able to access params directly without typescript errors you can cast the params Promise to this type
+ *
+ * ```tsx
+ * type Props = { params: Promise<{ id: string }>}
+ *
+ * export default async function Layout(props: Props) {
+ *  const directParams = (props.params as unknown as UnsafeUnwrappedParams<typeof props.params>)
+ *  return ...
+ * }
+ * ```
+ *
+ * This type is marked deprecated to help identify it as target for refactoring away.
+ *
+ * @deprecated
+ */
+export type UnsafeUnwrappedParams<P> = P extends Promise<infer U> ? Omit<U, 'then' | 'status' | 'value'> : never;
+/**
+ * @deprecated import specific root params from `next/root-params` instead.
+ */
+export declare function unstable_rootParams(): Promise<Params>;
+
+export declare function createParamsFromClient(underlyingParams: Params, workStore: WorkStore): Promise<Params>;
+export type CreateServerParamsForMetadata = typeof createServerParamsForMetadata;
+export declare const createServerParamsForMetadata: typeof createServerParamsForServerSegment;
+export declare function createServerParamsForRoute(underlyingParams: Params, workStore: WorkStore): Promise<Params>;
+export declare function createServerParamsForServerSegment(underlyingParams: Params, workStore: WorkStore): Promise<Params>;
+export declare function createPrerenderParamsForClientSegment(underlyingParams: Params): Promise<Params>;
+export declare function throwWithStaticGenerationBailoutError(route: string, expression: string): never;
+export declare function throwWithStaticGenerationBailoutErrorWithDynamicError(route: string, expression: string): never;
+export declare function throwForSearchParamsAccessInUseCache(workStore: WorkStore, constructorOpt: Function): never;
+export declare function isRequestAPICallableInsideAfter(): boolean;
+
